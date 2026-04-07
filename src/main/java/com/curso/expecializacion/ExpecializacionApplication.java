4851745d2c5c71650dd6b8b9414efc9ca9d4fcf0
David555acosta <148257651+David555acosta@users.noreply.github.com>
2026-04-07 01:20:57 -0300

    alojando en remoto

---
+++ src/main/java/com/curso/expecializacion/ExpecializacionApplication.java
@@ -0,0 +1,13 @@
+package com.curso.expecializacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpecializacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpecializacionApplication.class, args);
+	}
+
+}
