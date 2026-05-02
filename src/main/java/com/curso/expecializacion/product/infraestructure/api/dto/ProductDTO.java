4dd40d1fde03d37d1607fe49614fe1db3ca132c5
David Acosta <davidisrraelacostaz.9@gmail.com>
2026-05-02 18:26:55 -0300

    generando conexcion a db

--- src/main/java/com/curso/expecializacion/product/infraestructure/api/dto/ProductDTO.java
+++ src/main/java/com/curso/expecializacion/product/infraestructure/api/dto/ProductDTO.java
@@ -1,13 +1,6 @@
 package com.curso.expecializacion.product.infraestructure.api.dto;
-
-import jakarta.validation.constraints.DecimalMax;
-import jakarta.validation.constraints.DecimalMin;
 import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import org.hibernate.validator.constraints.Length;
-import org.hibernate.validator.constraints.NotBlank;
 
 @Data
 @RequiredArgsConstructor
