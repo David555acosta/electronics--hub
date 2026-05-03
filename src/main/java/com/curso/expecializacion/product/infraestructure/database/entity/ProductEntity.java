b83581f0f84a42eadfcdd7cc61173bf4c9bd0bdf
David555acosta <148257651+David555acosta@users.noreply.github.com>
2026-05-03 02:03:41 -0300

    creando product repository extendido de jpaRepository

--- src/main/java/com/curso/expecializacion/product/infraestructure/database/entity/ProductEntity.java
+++ src/main/java/com/curso/expecializacion/product/infraestructure/database/entity/ProductEntity.java
@@ -1,11 +1,18 @@
 package com.curso.expecializacion.product.infraestructure.database.entity;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.Getter;
 
 @Data
 @Getter
+@Table(name = "productos")
 public class ProductEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer codigo;
     private String nombre;
     private String descripcion;
---
+++ src/main/java/com/curso/expecializacion/product/infraestructure/database/repositoryDBProducts/QueryProductsRepository.java
@@ -0,0 +1,10 @@
+package com.curso.expecializacion.product.infraestructure.database.repositoryDBProducts;
+
+import com.curso.expecializacion.product.infraestructure.database.entity.ProductEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QueryProductsRepository extends JpaRepository<ProductEntity, Integer> {
+
+}
