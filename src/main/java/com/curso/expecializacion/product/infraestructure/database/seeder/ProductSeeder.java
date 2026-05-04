c82102ba365e662f56c3aa6fd9077720cd8adf7a
David Acosta <davidisrraelacostaz.9@gmail.com>
2026-05-04 18:45:15 -0300

    creando inserts automatizados

---
+++ src/main/java/com/curso/expecializacion/product/infraestructure/database/seeder/ProductSeeder.java
@@ -0,0 +1,41 @@
+package com.curso.expecializacion.product.infraestructure.database.seeder;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.curso.expecializacion.product.infraestructure.database.entity.ProductEntity;
+import com.curso.expecializacion.product.infraestructure.database.repositoryDBProducts.QueryProductsRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Component;
+
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class ProductSeeder implements CommandLineRunner {
+    private final QueryProductsRepository queryProductsRepository;
+    private final ResourceLoader resourceLoader;
+    private final ObjectMapper objectMapper;
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        long count = queryProductsRepository.count();
+
+        if (count == 0) {
+            Resource resource = resourceLoader.getResource("classpath:products.json");
+
+            List<ProductEntity> products = objectMapper.readValue(
+                    resource.getInputStream(),
+                    new TypeReference<List<ProductEntity>>() {}
+            );
+
+            queryProductsRepository.saveAll(products);
+        }
+
+    }
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO productos (nombre, descripcion, precio, imagen) VALUES
+                                                                ('Notebook Lenovo', 'Notebook 16GB RAM, SSD 512GB', 1200.50, 'lenovo.jpg'),
+                                                                ('Mouse Logitech', 'Mouse inalámbrico ergonómico', 25.99, 'mouse.jpg'),
+                                                                ('Teclado Redragon', 'Teclado mecánico RGB', 75.00, 'teclado.jpg'),
+                                                                ('Monitor Samsung', 'Monitor 24 pulgadas Full HD', 210.75, 'monitor.jpg'),
+                                                                ('Auriculares Sony', 'Auriculares con cancelación de ruido', 150.00, 'sony.jpg');
